75bcc089bcc2c0a0dcdc64e7b774b3cc3aaab732
Rohit Bachhav <bachhavrohit21@gmail.com>
2026-01-16 13:32:15 +0530

    requirements has been installed

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path:str) -> list[str]:
+    """This function will return the list of requirements"""
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="end_to_end_ml_cicd",
+    version="0.1.0",
+    author='Rohittt Bachhav',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
